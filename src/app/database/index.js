3ba28b58a4b3aa2788f55bd76304bc8d0988bdce
Omnath Shinde <omnathshinde1@gmail.com>
2026-01-18 22:14:59 +0530

    Use object freeze

--- src/app/database/index.js
+++ src/app/database/index.js
@@ -7,4 +7,5 @@ import models from "#db/models.js";
 const sequelize = new Sequelize(dbConfig);
 await hooks(sequelize);
 export const tables = await models(sequelize);
+Object.freeze(tables);
 export default sequelize;
