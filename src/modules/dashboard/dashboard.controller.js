1702af2a71a1e02ca1fbd044c8e78910c4bf8f9b
Omnath Shinde <omnathshinde1@gmail.com>
2026-06-13 09:07:46 +0530

    Update dashboard controller

--- src/modules/dashboard/dashboard.controller.js
+++ src/modules/dashboard/dashboard.controller.js
@@ -1,37 +1,80 @@
-import { Inward, Material } from "#src/models/index.js";
+import { Inward, Material, Picklist } from "#src/models/index.js";
 
 export const getAll = async (req, res) => {
-	const invetory = await Inward.count();
-	const material = await Material.count();
+	const [
+		totalInventory,
+		totalMaterials,
 
-	const qcPending = await Inward.count({
-		where: {
-			qcStatus: "Pending",
-		},
-	});
-	const qcTotal = await Inward.count({
-		where: {
-			qcStatus: "Pending",
-		},
-	});
-	const qcApproved = await Inward.count({
-		where: {
-			qcStatus: "Approved",
-		},
-	});
-	const qcRejected = await Inward.count({
-		where: {
-			qcStatus: "Rejected",
-		},
-	});
+		qcPending,
+		qcApproved,
+		qcRejected,
+
+		putawayPending,
+		putawayCompleted,
+
+		pickingPending,
+		pickingCompleted,
+
+		dispatched,
+		returned,
+
+		inStock,
+		outOfStock,
+	] = await Promise.all([
+		Inward.count(),
+		Material.count(),
+
+		Inward.count({ where: { qcStatus: "Pending" } }),
+		Inward.count({ where: { qcStatus: "Approved" } }),
+		Inward.count({ where: { qcStatus: "Rejected" } }),
+
+		Inward.count({ where: { isPutAway: false } }),
+		Inward.count({ where: { isPutAway: true } }),
+
+		Picklist.count({ where: { picklistStatus: "Pending" } }),
+		Picklist.count({ where: { picklistStatus: "Completed" } }),
+
+		Inward.count({ where: { isDispatch: true } }),
+		Inward.count({ where: { isReturn: true } }),
+
+		Inward.count({ where: { inStock: true } }),
+		Inward.count({ where: { inStock: false } }),
+	]);
+
 	const data = {
-		inventory: invetory,
-		material: material,
+		inventory: {
+			total: totalInventory,
+			inStock,
+			outOfStock,
+		},
+
+		materials: {
+			total: totalMaterials,
+		},
+
 		qc: {
-			total: qcTotal,
+			total: qcPending + qcApproved + qcRejected,
 			pending: qcPending,
 			approved: qcApproved,
 			rejected: qcRejected,
+		},
+
+		putaway: {
+			pending: putawayPending,
+			completed: putawayCompleted,
+		},
+
+		picking: {
+			pending: pickingPending,
+			completed: pickingCompleted,
+		},
+
+		dispatch: {
+			total: dispatched,
+		},
+
+		returns: {
+			total: returned,
 		},
 	};
 
